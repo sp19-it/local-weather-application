d8a47b91db9324abd7c1ea1b9bfb6a51e0e7c453
Shina Park <shinapark@Shinas-MacBook-Pro.local>
2020-02-03 14:52:59 -0800

    current-weather component and interface

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ICurrentWeather } from '../icurrent-weather';
+
+@Component({
+  selector: 'app-current-weather',
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.css']
+})
+export class CurrentWeatherComponent implements OnInit {
+  current: ICurrentWeather
+  constructor() {
+    this.current = {
+      city: 'Bethesda',
+      country: 'US',
+      date: 1485789600,
+      image: '',
+      temperature: 40,
+      description: 'sunny'
+    } as ICurrentWeather
+   }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/icurrent-weather.ts
@@ -0,0 +1,8 @@
+export interface ICurrentWeather {
+  city: string
+  country: string
+  date: number
+  temperature: number
+  description: string
+  image: string
+}
