8509dfceadd85ace11b1eacb96fa7e1c39ebef66
Shina Park <shinapark@Shinas-MacBook-Pro.local>
2020-02-08 13:31:21 -0800

    adding service and interface2

---
+++ src/app/weather.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { ICurrentWeatherData } from "./icurrent-weather-data";
+import { environment } from "src/environments/environment";
+
+@Injectable({
+  providedIn: "root"
+})
+export class WeatherService {
+  constructor(private httpClient: HttpClient) {}
+
+  getCurrentWeather(city: string, country: string) {
+    return this.httpClient.get<ICurrentWeatherData>(
+      `${environment.baseUrl}api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${environment.appId}`
+    );
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: '98736afd06c1d3e330c8f3adaac96015',
+  baseUrl: 'http://'
 };
 
 /*
